782901d1116bb2fb491d9d234ea2f27c23ddca6e
MakVM <verunchikmakarova2011@gmail.com>
2022-04-11 18:55:46 +0300

    it doesn't really find a local optimum, send help......

--- tsp_local/main.cpp
+++ tsp_local/main.cpp
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <vector>
 #include <iterator>
+#include <random>
 
 using namespace std;
 
@@ -43,6 +44,54 @@ float distance_cycle(vector<vector<int>> &graph, multimap<float,float> &points)
     return distance;
 }
 
+vector<vector<int>> changed_three(vector<vector<int>> graph)
+{
+    for(int c = 0; c<3; c++)
+    {
+        int random_vertex = rand() % graph.size();
+        int random_dest = rand() % graph.size();
+        
+        //making sure it doesn't lead to itself:
+        while (random_dest == random_vertex)
+        {
+            random_dest = rand() % graph.size();
+        }
+        
+        for(int j = 0; j<graph.size(); j++)
+        {
+            if(graph[random_vertex][j] == 1)
+            {
+                graph[random_vertex][j] = 0;
+            }
+        }
+        graph[random_vertex][random_dest] = 1;
+    }
+    
+    return graph;
+}
+
+bool still_hamiltonian(vector<vector<int>> &graph)
+{
+    //check that there's ONLY one 1 per column
+    for(int j = 0; j<graph.size(); j++)
+    {
+        int check = 0;
+        for(int i = 0; i<graph.size(); i++)
+        {
+            if(graph[i][j] == 1)
+            {
+                check ++;
+            }
+        }
+        if(check != 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 int main() {
 
     int n;
@@ -78,9 +127,37 @@ int main() {
 
     //getting the distance
     float distance = distance_cycle(graph, points);
+    float new_distance = distance;
     //cout<< distance<<endl;
     
-    
+    int count = 0;
+    while(count < pow(n,2))
+    {
+        graph = changed_three(graph);
+
+        if (still_hamiltonian(graph))
+        {
+            for(int i = 0; i<n; i++)
+            {
+                for(int j = 0; j<n; j++)
+                {
+        
+                    cout<<graph[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+            new_distance = distance_cycle(graph, points);
+        }
+        
+        if (new_distance < distance)
+        {
+            distance = new_distance;
+            count = 0;
+        }
+        count ++;
+    }
+    cout<<"The optimal length is:::"<<endl;
+    cout<<new_distance<<endl;
     
 //    multimap<float, float>::iterator itr;
 //    for (itr = points.begin(); itr != points.end(); ++itr)
