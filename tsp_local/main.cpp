6785466c1a661867efcc3653aa60c1556979d86f
MakVM <verunchikmakarova2011@gmail.com>
2022-04-11 15:45:32 +0300

    let's begin!

--- tsp_local/main.cpp
+++ tsp_local/main.cpp
@@ -1,14 +1,9 @@
-//
-//  main.cpp
-//  tsp_local
-//
-//  Created by Vera Makarova on 11.04.2022.
-//
+
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+
     return 0;
 }
