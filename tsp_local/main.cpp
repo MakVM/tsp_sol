e613ff93b0c2d57546cda2b9b4e8b604eed1446a
MakVM <verunchikmakarova2011@gmail.com>
2022-04-11 16:32:32 +0300

    multimap time

--- tsp_local/main.cpp
+++ tsp_local/main.cpp
@@ -1,9 +1,47 @@
 
 
 #include <iostream>
+#include <map>
+#include <math.h>
+
 using namespace std;
+
+
+int distance(pair<int,int> &p1, pair<int,int> &p2)
+{
+    int dist;
+    
+    dist = sqrt( pow((p1.first - p2.first),2) + pow((p1.second - p2.second),2) );
+    return dist;
+}
+
 
 int main() {
 
+    int n;
+    cin>> n;
+    //we need a shortest possible route that connects every vertex
+    
+    int x,y;
+    multimap<int,int> points;
+    
+    for(int i = 0; i<n;i++)
+    {
+        cin>> x;
+        cin>> y;
+        points.insert(pair<int, int> (x,y));
+        
+    } //automatically ordered by x-coordinate
+    
+    
+    
+    multimap<int, int>::iterator itr;
+    for (itr = points.begin(); itr != points.end(); ++itr) {
+            cout << '\t' << itr->first << '\t' << itr->second
+                 << '\n';
+        }
+    //cout distance(length of cycle) 0
+    //vertices numbered in the cycle??
+    
     return 0;
 }
