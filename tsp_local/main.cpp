b4f2175f978b035974ed7f5785412efdb947009b
MakVM <verunchikmakarova2011@gmail.com>
2022-05-08 15:09:32 +0300

    ya

--- tsp_local/main.cpp
+++ tsp_local/main.cpp
@@ -61,7 +61,7 @@ int main() {
 
     ofstream data;
     data.open ("my_data.txt");
-    data << "Test"<< "          "<<"MVS"<< "          "<<"VMM"<<"          "<<"Deviation\n";
+    data << "Test"<< "          "<<"MVS"<< "          "<<"VMM"<<"          "<<"Deviation (%)\n";
     //10 spaces
     
     std::ifstream MV_data("tsp_MV.txt");
@@ -74,84 +74,87 @@ int main() {
     for(std::string line; getline( MV_data, line );)
     {
         
-        if(count%2 == 0) //even -- string
-        {
-            name = line;
-            name.pop_back();
-            data<<line<<" ";
-            
-            std::ifstream input;
-            input.open (name, std::ifstream::in);
-            if (!input.is_open())
+            if(count%2 == 0) //even -- string
             {
-                    cerr << "Could not open the file - '"
-                         << name << "'" << endl;
-                    return EXIT_FAILURE;
-            }
-            
-            int n;
-            input>>n;
-            //cout<<n<<endl;
-            //we need a shortest possible route that connects every vertex
-        
-            float x,y;
-            multimap<float,float> points;
-        
-            for(int i = 0; i<n;i++)
-            {
-                input>> x;
-                input>> y;
-                points.insert(pair<float, float> (x,y));
-        
-            } //automatically ordered by x-coordinate
-        
-        
-            vector<int> vec(n);
-            //our initial guess-- ordered by x-coord
-            std::iota (std::begin(vec), std::end(vec), 0);
-        
-            //getting the distance
-            float distance = distance_vec(vec, points);
-        
-            float new_distance = distance;
-            int count = 0;
-            while(count< pow(n,2))
-            {
-                int random_place1 = rand() % vec.size();
-                int random_place2 = rand() % vec.size();
-                swap(vec[random_place1], vec[random_place2]);
-                new_distance = distance_vec(vec, points);
-                if (new_distance<distance)
+                name = line;
+                name.pop_back();
+                data<<line<<" ";
+                
+                std::ifstream input;
+                input.open (name, std::ifstream::in);
+                if (!input.is_open())
                 {
-        
-                    distance = new_distance;
-                    count = 0;
+                        cerr << "Could not open the file - '"
+                             << name << "'" << endl;
+                        return EXIT_FAILURE;
                 }
-                else //swap them back
+                
+                int n;
+                input>>n;
+                //cout<<n<<endl;
+                //we need a shortest possible route that connects every vertex
+            
+                float x,y;
+                multimap<float,float> points;
+            
+                for(int i = 0; i<n;i++)
                 {
+                    input>> x;
+                    input>> y;
+                    points.insert(pair<float, float> (x,y));
+            
+                } //automatically ordered by x-coordinate
+            
+            
+                vector<int> vec(n);
+                //our initial guess-- ordered by x-coord
+                std::iota (std::begin(vec), std::end(vec), 0);
+            
+                //getting the distance
+                float distance = distance_vec(vec, points);
+            
+                float new_distance = distance;
+                int count = 0;
+                while(count< pow(n,2))
+                {
+                    int random_place1 = rand() % vec.size();
+                    int random_place2 = rand() % vec.size();
                     swap(vec[random_place1], vec[random_place2]);
-                }
-        
-                count++;
-            }
-        
-            my_res = distance;
-            cout<<my_res<<endl;
-            input.close();
-            //get my_res
-        }
-        else //odd -- number
-        {
-            res = stof(line);
-            data<<line<<" ";
+                    new_distance = distance_vec(vec, points);
+                    if (new_distance<distance)
+                    {
             
-            //!!!
-            data <<"\n";
-        }
+                        distance = new_distance;
+                        count = 0;
+                    }
+                    else //swap them back
+                    {
+                        swap(vec[random_place1], vec[random_place2]);
+                    }
+            
+                    count++;
+                }
+            
+                my_res = distance;
+                //cout<<my_res<<endl;
+                input.close();
+                //get my_res
+            }
+            else //odd -- number
+            {
+                res = stof(line);
+                data<<line<<" ";
+                data<<my_res<<"         ";
+                
+                float dev = abs((my_res - res)/res * 100);
+                data<<dev;
+                data <<"\n";
+            }
+            
+            
+            
+            count++;
         
-        
-        
-        count++;
     }
     
     data.close();
